be095830ccb6da3c4980de59acc6931a1ba37234
woooyoung <axdsw121@gmail.com>
2021-11-16 09:30:26 +0900

    system exit, 리팩토링

--- src/board/dto/Article.java
+++ src/board/dto/Article.java
@@ -1,15 +1,15 @@
-package board;
+package board.dto;
 
 public class Article {
 
-	int id; // 게시물 번호
-	String title; // 게시물 번호
-	String body; // 게시물 번호
-	String regDate; // 게시물 등록 날짜
-	int hit; // 게시물 조회수
-	
+	public int id; // 게시물 번호
+	public String title; // 게시물 번호
+	public String body; // 게시물 번호
+	public String regDate; // 게시물 등록 날짜
+	public int hit; // 게시물 조회수
+
 	public Article(int id, String title, String body, String regDate) {
-		this(id,title,body,regDate,0);
+		this(id, title, body, regDate, 0);
 	}
 
 	public Article(int id, String title, String body, String regDate, int hit) {
